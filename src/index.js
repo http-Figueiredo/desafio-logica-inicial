b9f4c30671da43765ed2abba97c56230f6e691d3
http-Figueiredo <http.figueiredo@gmail.com>
2024-09-19 16:21:24 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-let charName = "";
-var rank = 0;
-
-function playerName() {
-    charName = document.getElementById("input").value;
-};
-
-if (rank >= 1 && rank <= 499) {
-    document.write("Your rank is Iron");
-}else if (rank >= 500 && rank <= 999) {
-    document.write("Your rank is Bronze");
-}else if (rank >= 1000 && rank <= 1499) {
-    document.write("Your rank is Silver");
-}else if (rank >= 1500 && rank <= 1999) {
-    document.write("Your rank is Gold");
-}else if (rank >= 2000 && rank <= 2499) {
-    document.write("Your rank is Platinum");
-}else if (rank >= 2500 && rank <= 2999) {
-    document.write("Your rank is Emerald");
-}else if (rank >= 3000 && rank <= 3499) {
-    document.write("Your rank is Diamond");
-}else if (rank >= 3500 && rank <= 3799) {
-    document.write("Your rank is Master");
-}else if (rank >= 3800 && rank <= 4199) {
-    document.write("Your rank is Grand Master");
-}else if (rank >= 4200) {
-    document.write("Your rank is Challenger");
-}else {
-    document.write("You\'re unranked, let\'s play some games");
+function askInfo() {
+    let playerName = prompt("diga o seu nome: ");
+    var rank = prompt("Digite quantos pontos você tem: (valor entre 0 e 4200+");
 }
-console.log(charName);
+
+let nameText = ("Hi, " + playerName + ", your rank is ");
+
+function playerRank() {
+    if (rank >= 1 && rank <= 499) {
+        document.write(nameText + "Iron");
+    }else if (rank >= 500 && rank <= 999) {
+        document.write(nameText + "Bronze");
+    }else if (rank >= 1000 && rank <= 1499) {
+        document.write(nameText + "Silver");
+    }else if (rank >= 1500 && rank <= 1999) {
+        document.write(nameText + "Gold");
+    }else if (rank >= 2000 && rank <= 2499) {
+        document.write(nameText + "Platinum");
+    }else if (rank >= 2500 && rank <= 2999) {
+        document.write(nameText + "Emerald");
+    }else if (rank >= 3000 && rank <= 3499) {
+        document.write(nameText + "Diamond");
+    }else if (rank >= 3500 && rank <= 3799) {
+        document.write(nameText + "Master");
+    }else if (rank >= 3800 && rank <= 4199) {
+        document.write(nameText + "Grand Master");
+    }else if (rank >= 4200) {
+        document.write(nameText + "Challenger");
+    }else {
+    document.write(nameText + "unranked! let\'s play some games :3");
+    }
+};
